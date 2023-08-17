4c15355a70d98fc15f0d011ce3e28e2decbe0890
pahancha <pahanchathuranga123@gmail.com>
2023-08-17 20:16:27 +0530

    home page: club cards, meetup cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,69 @@
-import Image from 'next/image'
-import RootLayout from './layout'
-import ClubCard from '../components/ClubCard'
-import MeetupCard from '../components/MeetupCard'
+import React from 'react';
+import ClubCard from '../components/ClubCard';
+import MeetupCard from '../components/MeetupCard';
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <>
-      <p>This is the home page</p>
-      <div className='flex justify-center items-center h-screen bg-gray-100' >
-        <ClubCard 
-          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-          clubName='Colombo Flutter Club (Sample)'
-          description='This a sample tech club which organizes tech meetups in flutter.'
+    <div className="p-4">
+      <p className="text-center mb-4">This is the home page</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ClubCard
+          imageUrl="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          clubName="Colombo Flutter Club (Sample)"
+          description="This a sample tech club which organizes tech meetups in flutter."
         />
-         <ClubCard 
-          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-          clubName='Colombo Flutter Club (Sample)'
-          description='This a sample tech club which organizes tech meetups in flutter.'
+        <ClubCard
+          imageUrl="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          clubName="Colombo Flutter Club (Sample)"
+          description="This a sample tech club which organizes tech meetups in flutter."
         />
-         <ClubCard 
-          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-          clubName='Colombo Flutter Club (Sample)'
-          description='This a sample tech club which organizes tech meetups in flutter.'
+        <ClubCard
+          imageUrl="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          clubName="Colombo Flutter Club (Sample)"
+          description="This a sample tech club which organizes tech meetups in flutter."
         />
-
-<ClubCard 
-          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-          clubName='Colombo Flutter Club (Sample)'
-          description='This a sample tech club which organizes tech meetups in flutter.'
+        <ClubCard
+          imageUrl="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          clubName="Colombo Flutter Club (Sample)"
+          description="This a sample tech club which organizes tech meetups in flutter."
         />
-
-<ClubCard 
-          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-          clubName='Colombo Flutter Club (Sample)'
-          description='This a sample tech club which organizes tech meetups in flutter.'
+        <ClubCard
+          imageUrl="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          clubName="Colombo Flutter Club (Sample)"
+          description="This a sample tech club which organizes tech meetups in flutter."
         />
       </div>
 
-      <MeetupCard />
-    </>
-  )
-}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <MeetupCard 
+          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          clubName='Colombo Flutter Meetup (Sample)'
+          description='This a sample tech club which organizes tech meetups in flutter.'
+        />
+            <MeetupCard 
+          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          clubName='Colombo Flutter Meetup (Sample)'
+          description='This a sample tech club which organizes tech meetups in flutter.'
+        />
+            <MeetupCard 
+          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          clubName='Colombo Flutter Meetup (Sample)'
+          description='This a sample tech club which organizes tech meetups in flutter.'
+        />
+            <MeetupCard 
+          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          clubName='Colombo Flutter Meetup (Sample)'
+          description='This a sample tech club which organizes tech meetups in flutter.'
+        />
+            <MeetupCard 
+          imageUrl='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          clubName='Colombo Flutter Meetup (Sample)'
+          description='This a sample tech club which organizes tech meetups in flutter.'
+        />
+
+        </div> 
+    </div>
+  );
+};
+
+export default Home;
--- src/components/MeetupCard.tsx
+++ src/components/MeetupCard.tsx
@@ -1,9 +1,21 @@
-const MeetupCard = () => {
-    return(
-        <>
-        <h3>This is a meetup card.</h3>
-        </>
-    );
+import React from 'react';
+import Image from 'next/image';
+import autoprefixer from 'autoprefixer';
+
+interface MeetupCardProps {
+  imageUrl: string;
+  clubName: string;
+  description: string;
+}
+
+const MeetupCard: React.FC<MeetupCardProps> = ({ imageUrl, clubName, description }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4 mx-auto max-w-xs">
+      <Image width={500} height={250} src={imageUrl} alt={`Logo of ${clubName}`} className="mx-auto mb-4" />
+      <h3 className="text-xl font-semibold mb-2">{clubName}</h3>
+      <p className="text-gray-600">{description}</p>
+    </div>
+  );
 };
 
 export default MeetupCard;
