778e3862659af2bd2a4229f83bf598cde632fa63
pahancha <pahanchathuranga123@gmail.com>
2023-08-23 20:54:59 +0530

    single club and single meetup

--- src/components/sections/SingleClub.tsx
+++ src/components/sections/SingleClub.tsx
@@ -1,14 +1,35 @@
 import { SingleClubProps } from "@/src/util/types/SingleComponentProp";
+import Image from "next/image";
 
-export default function SingleClub({ id } :SingleClubProps) {
-    // const id = params.id;
+export default function SingleClub({ id }: SingleClubProps) {
 
-    return(
-        <>
-        <h1>testing the Club {id} in section</h1>
-        <h1>
-            Club information for {id} will display here.
-        </h1>
-        </>
-    );
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl font-semibold content-center">
+        testing the Club {id} in section
+      </h1>
+      <Image
+        width={750}
+        height={250}
+        src="https://images.pexels.com/photos/4974920/pexels-photo-4974920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt={`Logo of title will go here`}
+        className="mx-auto mb-4 py-10"
+      />
+      <h3 className="text-2xl text-emerald-600 mb-4">
+        Type of the Club will display here
+      </h3>
+      <div className="flex space-x-4 mb-8">
+        <p>Start time</p>
+        <p>End time</p>
+      </div>
+      <div className="flex space-x-4 mb-8">
+        <p>Created time</p>
+        <p>Updated time</p>
+      </div>
+
+      <h1 className="text-3xl font-semibold">
+        Club information for {id} will display here.
+      </h1>
+    </div>
+  );
 }
--- src/components/sections/SingleMeetup.tsx
+++ src/components/sections/SingleMeetup.tsx
@@ -1,14 +1,35 @@
 import { SingleMeetupProps } from "@/src/util/types/SingleComponentProp";
+import Image from "next/image";
 
-export default function SingleMeetup({ id } :SingleMeetupProps) {
-    // const id = params.id;
+export default function SingleMeetup({ id }: SingleMeetupProps) {
 
-    return(
-        <>
-        <h1>testing the Meetup {id} in section</h1>
-        <h1>
-            Meetup information for {id} will display here.
-        </h1>
-        </>
-    );
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl font-semibold content-center">
+        testing the Meetup {id} in section
+      </h1>
+      <Image
+        width={750}
+        height={250}
+        src="https://images.pexels.com/photos/4974920/pexels-photo-4974920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt={`Logo of title will go here`}
+        className="mx-auto mb-4 py-10"
+      />
+      <h3 className="text-2xl text-emerald-600 mb-4">
+        Type of the meetup will display here
+      </h3>
+      <div className="flex space-x-4 mb-8">
+        <p>Start time</p>
+        <p>End time</p>
+      </div>
+      <div className="flex space-x-4 mb-8">
+        <p>Created time</p>
+        <p>Updated time</p>
+      </div>
+
+      <h1 className="text-3xl font-semibold">
+        Meetup information for {id} will display here.
+      </h1>
+    </div>
+  );
 }
