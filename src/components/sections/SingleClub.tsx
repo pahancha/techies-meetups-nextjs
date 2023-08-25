2082601e09de397d4185003e90fdf8677786f2b9
pahancha <pahanchathuranga123@gmail.com>
2023-08-25 10:41:34 +0530

    events(meetups) list inside the relavent club.

--- src/components/sections/SingleClub.tsx
+++ src/components/sections/SingleClub.tsx
@@ -1,37 +1,58 @@
 import { SingleClubProps } from "@/src/util/types/SingleComponentProp";
 import Image from "next/image";
 import { getSingleClubDetails } from "@/src/util/club-api";
+import MeetupCard from "../MeetupCard";
+import { MeetupType } from "@/src/util/types/MeetupType";
 
 export default async function SingleClub({ id }: SingleClubProps) {
   const club = await getSingleClubDetails(id);
 
   return (
-
     <div className="flex flex-col items-center">
       {club && (
-        <h1 className="text-5xl font-semibold content-center">
-          testing the Meetup {id} in section
-        </h1>
+        <>
+          <h1 className="text-5xl font-semibold content-center">
+            testing the Meetup {id} in section
+          </h1>
+          <h1 className="text-5xl font-semibold content-center">
+            testing the Meetup {club?.name} in section
+          </h1>
+          <Image
+            width={750}
+            height={250}
+            src={club?.photoURL}
+            alt={`Logo of ${club?.name}`}
+            className="mx-auto mb-4 py-10"
+          />
+          <h3 className="text-2xl text-emerald-600 mb-4">
+            Type of the Club will display here
+          </h3>
+          <div className="flex space-x-4 mb-8">
+            <p className="text-gray-600">
+              Created On: {new Date(club?.createdOn).toLocaleString()}
+            </p>
+            <p className="text-gray-600">
+              Updated On: {new Date(club?.updatedOn).toLocaleString()}
+            </p>
+          </div>
+          <h4> Events list</h4>
+          <ul>
+            {club.events.map((event: MeetupType) => (
+              <li key={event.id}>
+                <MeetupCard
+                  id={event.id}
+                  photoURL={event.photoURL}
+                  name={event.name}
+                  startTime={event.startTime}
+                  endTime={event.endTime}
+                  createdOn={event.createdOn}
+                  updatedOn={event.updatedOn}
+                />
+              </li>
+            ))}
+          </ul>
+        </>
       )}
-        <h1 className="text-5xl font-semibold content-center">
-          testing the Meetup {club?.name} in section
-        </h1>
-      <Image
-        width={750}
-        height={250}
-        src={club?.photoURL}
-        alt={`Logo of ${club?.name}`}
-        className="mx-auto mb-4 py-10"
-      />
-      <h3 className="text-2xl text-emerald-600 mb-4">
-        Type of the Club will display here
-      </h3>
-
-      <div className="flex space-x-4 mb-8">
-      <p className="text-gray-600">Created On: {new Date(club?.createdOn).toLocaleString()}</p>
-          <p className="text-gray-600">Updated On: {new Date(club?.updatedOn).toLocaleString()}</p>
-      </div>
-
       <h1 className="text-3xl font-semibold">
         Club information for club with ID {id} will display here.
       </h1>
