5b14b1e18b5840b3a97b96af52c9457cc098c6f6
pahancha <pahanchathuranga123@gmail.com>
2023-10-10 17:35:15 +0530

    styling: create new club and create new meetup buttons and some adjustments.

--- src/app/user/user-board/page.tsx
+++ src/app/user/user-board/page.tsx
@@ -55,12 +55,20 @@ const UserDashboard: React.FC = () => {
   }
 
   return (
-    <div className="p-6 bg-white">
-      <h1 className="text-2xl font-semibold mb-4">User Dashboard</h1>
+    <div className="p-10 bg-white">
+      <h1 className="text-3xl font-semibold mb-4">User Dashboard</h1>
       <div className="mb-4">
         <p>
           Username: <span className="font-bold">{userInfo.username}</span>
         </p>
+        <div className="mt-4 flex space-x-4">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Create New Club
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Create New Meetup
+          </button>
+        </div>
       </div>
       <div>
         <h2 className="text-xl font-semibold mb-2">Clubs</h2>
