9ce68a0e89e2d0c85b379df133f90178fe6fad6e
pahancha <pahanchathuranga123@gmail.com>
2023-10-10 13:02:57 +0530

    feat: dynamic user dashboards.

--- src/app/user/user-board/page.tsx
+++ src/app/user/user-board/page.tsx
@@ -1,29 +1,22 @@
 'use client'
 
 import React, { useState, useEffect } from "react";
-import { ClubType } from "@/src/util/types/ClubType";
-import { MeetupType } from "@/src/util/types/MeetupType";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 
 const UserDashboard: React.FC = () => {
   const { data: session } = useSession();
 
-  const [info, setInfo] = useState<{
-    numberOfClubs: number;
-    numberOfMeetups: number;
-    clubs: ClubType[];
-    meetups: MeetupType[];
-  } | null>(null);
+  const [userInfo, setUserInfo] = useState<any>(null);
 
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const res = await fetch("http://localhost:8080/api/user/info", {
+        const res = await fetch(`http://localhost:8080/api/user/info/${session?.id}`, {
           method: "GET",
-        //   headers: {
-        //     Authorization: `Bearer ${session?.jwt}`,
-        //   },
+          headers: {
+            Authorization: `Bearer ${session?.jwt}`,
+          },
         });
 
         if (!res.ok) {
@@ -31,7 +24,7 @@ const UserDashboard: React.FC = () => {
         }
 
         const response = await res.json();
-        setInfo(response);
+        setUserInfo(response);
       } catch (error) {
         console.error("Error fetching user information:", error);
       }
@@ -44,11 +37,19 @@ const UserDashboard: React.FC = () => {
     // Implement club removal logic here
   };
 
+  const handleUpdateClub = (clubId: number) => {
+    // Implement club update logic here
+  };
+
   const handleRemoveMeetup = (meetupId: number) => {
     // Implement meetup removal logic here
   };
 
-  if (!info) {
+  const handleUpdateMeetup = (meetupId: number) => {
+    // Implement meetup update logic here
+  };
+
+  if (!userInfo) {
     // Loading state
     return <div>Loading...</div>;
   }
@@ -58,45 +59,58 @@ const UserDashboard: React.FC = () => {
       <h1 className="text-2xl font-semibold mb-4">User Dashboard</h1>
       <div className="mb-4">
         <p>
-          Number of Clubs Created:{" "}
-          <span className="font-bold text-xl">{info.numberOfClubs}</span>
-        </p>
-        <p>
-          Number of Meetups Created:{" "}
-          <span className="font-bold text-xl">{info.numberOfMeetups}</span>
+          Username: <span className="font-bold">{userInfo.username}</span>
         </p>
       </div>
       <div>
         <h2 className="text-xl font-semibold mb-2">Clubs</h2>
         <ul>
-          {info.clubs.map((club) => (
-            <li
-              key={club.id}
-              className="flex items-center justify-between py-2 border-b"
-            >
+          {userInfo.clubs.map((club: any) => (
+            <li key={club.id} className="py-2 border-b flex items-center justify-between">
               <Link href={`/clubs/${club.id}`}>
                 <span>{club.title}</span>
               </Link>
-              {/* You can add a button here to remove the club */}
+              <div className="flex space-x-2">
+                <button
+                  onClick={() => handleRemoveClub(club.id)}
+                  className="text-red-500 hover:text-red-700"
+                >
+                  Remove
+                </button>
+                <button
+                  onClick={() => handleUpdateClub(club.id)}
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Update
+                </button>
+              </div>
             </li>
           ))}
         </ul>
-        {/* Add a button here to create a new club */}
       </div>
       <div>
-        <h2 className="text-xl font-semibold my-2">Meetups</h2>
+        <h2 className="text-xl font-semibold mb-2">Meetups</h2>
         <ul>
-          {info.meetups.map((meetup: MeetupType) => (
-            <li
-              key={meetup.id}
-              className="flex items-center justify-between py-2 border-b"
-            >
-              <span>{meetup.name}</span>
-              {/* You can add a button here to remove the meetup */}
+          {userInfo.clubs.map((club: any) => (
+            <li key={club.id} className="py-2 border-b flex items-center justify-between">
+              <span>{club.title}</span>
+              <div className="flex space-x-2">
+                <button
+                  onClick={() => handleRemoveClub(club.id)}
+                  className="text-red-500 hover:text-red-700"
+                >
+                  Remove
+                </button>
+                <button
+                  onClick={() => handleUpdateClub(club.id)}
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Update
+                </button>
+              </div>
             </li>
           ))}
         </ul>
-        {/* Add a button here to create a new meetup */}
       </div>
     </div>
   );
