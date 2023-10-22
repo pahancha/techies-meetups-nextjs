fb8633abbb19e3fb8405b780cffaff9e589bbe5e
pahancha <pahanchathuranga123@gmail.com>
2023-10-22 18:30:49 +0530

    docs: elaborated middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,42 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-
 export default withAuth(
-    function middleware(req) {
-        console.log("User token: " +  req.nextauth.token);
+  function middleware(req) {
 
-        if(req.nextUrl.pathname.startsWith("/admin/") && req.nextauth.token?.role!=="[ADMIN]")
-        return NextResponse.rewrite(new URL("/auth/signin?message=You are not authorized", req.url))
+    // Log the user's token to the console
+    console.log("User token: " + req.nextauth.token);
 
-        
-    },
-    {
-        callbacks: {
-            authorized: ({token}) => !!token, 
-        }
+    // Check if the request path starts with "/admin/" and the user's role is not "[ADMIN]" 
+    if (
+      req.nextUrl.pathname.startsWith("/admin/") &&
+      req.nextauth.token?.role !== "[ADMIN]"
+    ) {
+      // If the conditions are met, rewrite the request to the "/auth/signin" URL with a message
+      return NextResponse.rewrite(
+        new URL("/auth/signin?message=You are not authorized", req.url)
+      );
     }
-)
 
-export const config = { matcher: ["/admin/:path*","/user/:path*"] };
+    // Check if the request path starts with "/user/" and the user's role is "[USER]"
+    
+    if (
+      req.nextUrl.pathname.startsWith("/user/") &&
+      req.nextauth.token?.role !== "[USER]"
+    ) {
+      // If the conditions are met, rewrite the request to the "/auth/signin" URL with a message
+      return NextResponse.rewrite(
+        new URL("/auth/signin?message=You are not authorized", req.url)
+      );
+    }
+  },
+  {
+    callbacks: {
+      // Check if the user is authorized based on their token
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+// Export the configuration for the middleware
+export const config = { matcher: ["/admin/:path*", "/user/:path*"] };
